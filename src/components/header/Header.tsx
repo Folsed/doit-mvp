b6f867d0f171283d2912c5be6ab905eef105cdcc
Kyrylo <dexter37814@gmail.com>
2025-05-06 13:17:57 +0300

    feat: init redux store

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,13 +4,23 @@ import MenuIcon from '@mui/icons-material/Menu'
 import { useState } from 'react'
 import DrawerList from './DrawerList'
 import ToggleThemeButton from '../ui/ToggleThemeButton'
+import { usePathname } from 'next/navigation'
+
+const _headerTitle = [
+    { path: '/', title: 'DOiT MVP' },
+    { path: '/posts', title: 'Усі пости' },
+    { path: '/posts/create', title: 'Створити пост' },
+]
 
 const Header = () => {
+    const pathname = usePathname()
     const [open, setOpen] = useState<boolean>(false)
 
     const toggleDrawer = (newOpen: boolean) => () => {
         setOpen(newOpen)
     }
+
+    const currentTitle = _headerTitle.find(item => item.path === pathname)
 
     return (
         <Box sx={{ flexGrow: 1 }}>
@@ -30,7 +40,7 @@ const Header = () => {
                         <DrawerList toggleDrawer={toggleDrawer} />
                     </Drawer>
                     <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
-                        DOiT MVP
+                        {currentTitle?.title}
                     </Typography>
                     <ToggleThemeButton />
                 </Toolbar>
--- src/providers/Providers.tsx
+++ src/providers/Providers.tsx
@@ -1,10 +1,13 @@
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter'
 import MUIThemeProvider from './MUIThemeProvider'
+import StoreProvider from './StoreProvider'
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
     return (
         <AppRouterCacheProvider>
-            <MUIThemeProvider>{children}</MUIThemeProvider>
+            <StoreProvider>
+                <MUIThemeProvider>{children}</MUIThemeProvider>
+            </StoreProvider>
         </AppRouterCacheProvider>
     )
 }
